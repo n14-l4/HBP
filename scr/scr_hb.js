60d98abdbad5395fa21a45f556ab0196c297b0af
39l6 <130191016+39l6@users.noreply.github.com>
2024-05-07 22:22:24 -0600

    Update scr_hb.js

--- scr/scr_hb.js
+++ scr/scr_hb.js
@@ -1,4 +1,8 @@
+document.title = 'hist0r1as brev3s pachec4s';
+let elTitul = document.createElement('div');
+elTitul.innerText = hb.sentir[0] + hb.sentir[1];
 let elBody = document.createElement('div');
-elBody.innerText = hb.sentir;
+elBody.innerText = hb.sentir[2];
 
+document.body.appendChild(elTitul);
 document.body.appendChild(elBody);

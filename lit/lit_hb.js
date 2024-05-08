79153ebd8de2630a10900158d24ebbf3a303703d
39l6 <130191016+39l6@users.noreply.github.com>
2024-05-07 21:48:51 -0600

    Create lit_hb.js

---
+++ lit/lit_hb.js
@@ -0,0 +1,41 @@
+let hb = {};
+
+hb.sentir = [ 'elSentir', 'v0.0.6',
+  `
+    En el oceano, aparetemente grande, o para mis sentidos un lugar donde no ves la costa.
+    Sientes que las olas se mueven, al principio con mucho interes, con colores vivos y distintos, esos 
+    pigmentos aglomerados que artistas muestran con trazo de lluvia.
+    Las estrellas se muestran el sol despierta y me las esconde pero se que están ahí, en este calor que me
+    esconde la frescura, me permite soñar despierto, a veces remando
+    con fuerza otras esperando a que añochezca, pero sin querer uno va perdiendo el destino.
+    Sabes que en las noche esas luces te guian a cualquier lugar, unas veces decides el ir por un lado, remas y remas
+    con fuerza, pero el recuerdo te hace mirar atrás y te guias por otras luces ya que en el cansancio, en ese va y ven
+    desconocido, en el respiro, en ese pensar posible, te genera una inseguridad, una pequeña particula para agrandecer solo para volver a esa seguridad enmascarada.
+    En esa vuelta donde ya la frustración es la compañera, te quedas con el soñar, en el mañana se resolvera.
+    Pasan no los dias ni las semanas, sino años, años que sin querer se fueron como meses. Los gritos ya no funcionan, la victimización tampoco. No importa ya
+    el como se llego, o cual era el destino, ese agua del alrededor pasa a ser el aburrimiento, el odio, el apego por pensar. La belleza se esconde para dar paso 
+    a la costumbre y de ella al olvido.
+    Uno olvida la pasión que tubo al contruir ese navío, el telar ese vela el fruto o recompensa de un esfuerzo un sentirse agusto haciendo cualquier acto.
+    En las lluvias más duras uno puede preguntarse porque, como ese egoismo aparente donde el afan de uno se convierte en un adios de tantos. En la costa de donde 
+    partiste, el tiempo en el entorno que te moldeo, esas formas que sin querer te das cuenta que estan en ti. En ese pozo dentro del mar donde ni la lluvia ya te permite
+    ver esas luces que te guiaban, ese odio, se convierte en amor, ppr cada gota un recuerdo, el recuerdo de poder haber estado ahí, el sentirse agusto con los que
+    te amaron, ese amor de entender o tratar de ver ese moldeo que me hizo ser. Podria haber hecho las cosas mejor, ayudado más, entender a los demas sin ser tan egoista. 
+    Lo bueno que hiciera no desaparece, pero el ego de solo ver eso me aburre ademas empiezo a creer en ver las que deveria haber hecho mejor.
+    Amistades, familia, parejas.
+    Ahora se, el sol se despierta de nuevo, y hay que creer en uno para poder demostrar sin tratar de convencer o demás.
+    En la ayuda uno esconde el sentirse limitado, en la sociedad el quedarse con las palmadas y la identidad de nuestro contexto. Por lo tanto Hay que estar en el oceano, para entender y creer en uno.
+    En ese nuevo sentir ahora esta el luchar o el atreverse sin limitarse.
+    Uno puede ahora esperar no depender, pero lo mas importante es el entender el laverinto de impulsos que querran que vuelvas a la normalidad.
+    El elegir una meta, miedo a perder otras cuando en la constancia sabes que nada te pararia.
+    Por lo tanto volviendo a la sociedad, el poder interactuar con una persona compartiendo el tiempo de vida de ambos, es importante, el resultado de eso es el mayor de mis respetos.
+    Pero me encuentro en la soledad, por cada paso decisivo dejo de remar, sin querer el recurrir al pensar, desde el pensar pasado hasta el presente categorizado. Y es que el pasado te moldea, pero en esa 
+    costumbre uno moldea a los demas, hay que desamoldar mas bien a uno mismo, en el observar aunque sea la misma ola, con el mismo delfin no significa que vaya a nadar al mismo lugar.
+    Por eso te vas deteniendo, en ese oceano que recuerdas te gustaba observar. En la observación, uno encuentra nuevas categorias mas bien experiencias sin nombre ni valor, sino un sentir nuevo, un conjuto
+    armonioso en el cual esta uno presente.
+    Es una herramienta olvidada por el contexto, pero que se puede reaprender, de un modo u otro nuestros antepasados a raiz de ello, ahora podemos creer y entender como llamar este mundo.
+    Entonces mas que anteponer cualquier conocimiento es entender que ese conocimiento pertenece a un lugar y tiempo, vivido y sentido como quizas yo este sintiendo y viviendo este observar.
+    No quieres ya ser famosos, o venderte, sino en unas metas casi tan abismales que solo por afan para ayudar a todos las quieres hacer.
+    Pero la rutina esa costumbre, ese luchar, el laberinto, te recuerda que ya tenias una idea un lugar a donde ir. Entras en el recurrir, en el bucle interno del querer poder, el encerrarse en ideas ya no recuerdos.
+    Solo tengo que seguir remando, aparentemente ya nada me detendria, el unico que se detiene ahora mismo es uno mismo.
+    Me siento el mismo a pesar de que ya no siento igual, pero  si remara como podria llegar a sentirme?
+  ` ];

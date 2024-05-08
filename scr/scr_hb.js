b8fb5055a6406bf72dff0aba8883ff4dc9103696
39l6 <130191016+39l6@users.noreply.github.com>
2024-05-07 22:47:13 -0600

    Update scr_hb.js

--- scr/scr_hb.js
+++ scr/scr_hb.js
@@ -1,8 +1,12 @@
 document.title = 'hist0r1as brev3s pachec4s';
-let elTitul = document.createElement('div');
+let elTitul = document.createElement('h6');
+elTitul.style.fontFamily = '"Lucida Console", "Courier New", monospace';
 elTitul.innerText = hb.sentir[0] + hb.sentir[1];
 let elBody = document.createElement('div');
+elBody.style.fontFamily = '"Lucida Console", "Courier New", monospace';
 elBody.innerText = hb.sentir[2];
 
 document.body.appendChild(elTitul);
 document.body.appendChild(elBody);
+
+//TODO funct el(elTyp,cntnt,css) return append
